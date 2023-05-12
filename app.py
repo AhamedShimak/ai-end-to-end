f24594678dc08233d2bafab8b16a9f23aca2d725
Shimak Hanifa <64198823+AhamedShimak@users.noreply.github.com>
2023-05-12 01:07:58 +0000

    for_the_deploymrnt

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 from utils import generate_image,summerize
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 st.title("Mute baby")
-st.header("Yeah! I can't speak :( but I can show ;)")
+
+st.header("Yeah! I can't speak....but I can show ")
 
 text=st.text_area("You can speak :)")
 
@@ -15,4 +22,4 @@ if st.button("Now you go!"):
         with st.spinner("Hmmmm Lemme say... OOOOPS Show!!"):
             image= generate_image(summary)
         st.info(f"summery:{summary}")
-        st.image(image)
+        st.image(image) 
